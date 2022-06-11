ef8c7e8cc19db578310f2e73fe6b2b2ecdf39481
TotallyNewGuy <lv.zh@northeastern.edu>
2022-06-11 12:32:15 -0700

    add source cmd

--- grpc-order/start.sh
+++ grpc-order/start.sh
@@ -3,6 +3,7 @@
 set -e
 
 echo "run db migration"
+source /app/app.env
 /app/migrate -path /app/migration -database "postgresql://root:AppleMan922@postgres:5432/book_store?sslmode=disable" -verbose up
 
 echo "start the app"
